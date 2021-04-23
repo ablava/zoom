c9b1fd5f92ac0b92f00459b5d673b8af94208e65
Alexandr Ablovatski <ablovatski@gmail.com>
2021-04-23 08:33:25 -0400

    Create zoom_settings.py

---
+++ zoom_settings.py
@@ -0,0 +1,9 @@
+"""
+This config file contains constants describing 
+the specific environment where this script is used.
+"""
+
+# API KEY (use your own)
+API_KEY = ''
+# API SECRET (use your own)
+API_SEC = ''
